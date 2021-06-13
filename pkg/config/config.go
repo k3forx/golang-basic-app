da99cae6b9094c9ae742812022a8c5e5b98a6f37
Kanata Miyahana <afiri.bupa@gmail.com>
2021-06-13 23:58:42 +0900

    Introduce a package for session management (#14)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,11 +1,17 @@
 package config
 
 import (
+	"log"
 	"text/template"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 // AppConfig holds the application config
 type AppConfig struct {
 	UseCache      bool // false is development mode as cache is reloaded every time
 	TemplateCache map[string]*template.Template
+	InfoLog       *log.Logger
+	InProduction  bool
+	Session       *scs.SessionManager
 }
